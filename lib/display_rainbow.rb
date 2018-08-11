0af144772d159405ade3e643d33fea24ef94d6fe
Alianny Melina Perez <amialejan@gmail.com>
2018-08-11 16:55:30 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,3 +4,4 @@ def display_rainbow (color_array)
   
 #want "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B:            #{color_array[4]}, #{color_array[5]}, V: #{color_array[6]}"
+end
